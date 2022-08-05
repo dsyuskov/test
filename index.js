6e6094b06b8a8f5989c8bc00bdc7c46b45fa6344
dsyuskov <yuskovss@gmail.com>
2022-08-05 09:46:46 +0400

    init

---
+++ index.js
@@ -0,0 +1,64 @@
+const data = [
+  {
+    family: 'Ivanovy',
+    members: [
+      {
+        name: 'Ivan',
+        age: '35',
+        type: 'Father'
+      },
+      {
+        name: 'Mariya',
+        age: '34',
+        type: 'Mother'
+      },
+      {
+        name: 'Anna',
+        age: '5',
+        type: 'Daughter'
+      }
+    ]
+  },
+  {
+    family: 'Petrovy',
+    members: [
+      {
+        name: 'Anton',
+        age: '50',
+        type: 'Father'
+      },
+      {
+        name: 'Ekaterina',
+        age: '53',
+        type: 'Mother'
+      },
+    ]
+  },
+  {
+    family: 'Sidorovy',
+    members: [
+      {
+        name: 'Dmitry',
+        age: '35',
+        type: 'Father'
+      },
+      {
+        name: 'Venus',
+        age: '35',
+        type: 'Mother'
+      },
+      {
+        name: 'Ivan',
+        age: '10',
+        type: 'Son'
+      },
+      {
+        name: 'Kirill',
+        age: '5',
+        type: 'Son'
+      }
+    ]
+  },
+]
+
+console.log(`data`, data);
